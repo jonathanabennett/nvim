e6fb61985cfa2f2e67921c03ca193aad21ab8bab
Jonathan Bennett <doulos05@gmail.com>
2024-09-02 14:38:36 +0900

    Modularize Kickstart configuration.

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,3 +1,5 @@
+-- See `:help gitsigns` to understand what the configuration keys do
+
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   opts = {
