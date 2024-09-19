36b3a651787e601991c88216dfde68b7dac8fffb
Jonathan Bennett <doulos05@gmail.com>
2024-09-19 14:08:11 +0900

    Update keymaps.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,7 +3,7 @@
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
@@ -32,3 +32,6 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.keymap.set('n', 'Q', '<nop>')
+
+-- This keybinding deletes what's selected and pastes while preserving the yank register.
+vim.keymap.set('x', '<leader>p', [["_dP]])
