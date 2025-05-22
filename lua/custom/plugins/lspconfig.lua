c8766139a3f8677191f222e8265ba44edd0dea60
Jonathan Bennett <doulos05@gmail.com>
2025-05-22 21:43:48 +0900

    Swap from nvim-cmp to blink in lspconfig

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -13,7 +13,7 @@ return {
     { 'j-hui/fidget.nvim', opts = {} },
 
     -- Allows extra capabilities provided by nvim-cmp
-    'hrsh7th/cmp-nvim-lsp',
+    'saghen/blink.cmp',
   },
   config = function()
     -- Brief aside: **What is LSP?**
@@ -43,11 +43,8 @@ return {
 
     -- LSP servers and clients are able to communicate to each other what features they support.
     --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-    --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
+    --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
     --
@@ -130,6 +127,7 @@ return {
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+    -- TODO: Update whole Mason config to match Kickstart. It's currently too confused.
     require('mason-lspconfig').setup {
       ensure_installed = {},
       automatic_enable = false,
